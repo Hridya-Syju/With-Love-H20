25030e36d4768ca31cdaed6e2365c70b9f52ec4d
123BENEETA <beneetabency566@gmail.com>
2024-04-17 21:05:01 +0530

    added about us

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import WaterFpCalc from './User/footrprint';
 import LoginForm from './login';
 import SignupPage from './signup';
 import ProfilePage from './User/userprofile';
+import AboutUsPage from './User/aboutus';
 
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
@@ -20,6 +21,7 @@ function App() {
           <Route path="/footrprint" element={<WaterFpCalc />} />
           <Route path="/" element={<LoginForm />} />
           <Route path="/userprofile" element={<ProfilePage/>}/>
+          <Route path="/aboutus"element={<AboutUsPage/>}/>
         </Routes>
       </Router>
     );
